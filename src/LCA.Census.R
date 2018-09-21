ba3e2cdd31d124a62010d671ac768e3146c6b468
CORP\agmad <agmad.kafaar@webafrica.com>
2018-09-21 17:18:26 +0200

    heatmaps + scripts to genenate heatmaps

--- src/LCA.Census.R
+++ src/LCA.Census.R
@@ -10,12 +10,30 @@ library("poLCA")
 
 fftempdir = "~/GitHub/Btech2018/data/Tempff"
 
-#perform PCA to identify which columns explain the highest variance
+#load initial dataset
 census <- read.csv("2001.Combined.Dataset.Coded.csv")
+
+#do some cluster analysis
+#compute distance between first 20 observations
+distance <-dist(census[1:20,-1])
+
+# get matrix of distance
+distance <- data.matrix(distance)
+#get dimensions of distance object
+dim<- ncol(distance)
+
+#create a heatmap
+#first 20 observations as axis
+image(1:dim, 1:dim, distance, axes = FALSE, xlab="", ylab="")
+axis(1, 1:dim, census[1:20,1], cex.axis = 0.5, las=3)
+axis(2, 1:dim, census[1:20,1], cex.axis = 0.5, las=1)
+#add distance vals to heatmap
+text(expand.grid(1:dim, 1:dim), sprintf("%0.1f", distance), cex=0.6)
 
 #remove ward from dataframe
 census$Ward <- NULL
 
+#perform PCA to identify which columns explain the highest variance
 #perform PCA and set scale to true
 pca <- prcomp(census, scale. = TRUE)
 
